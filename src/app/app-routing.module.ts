4f1df272ba7fbfbaaad3201d53550f4c6cc3df7b
Fauzan Fathurrahman <fauzan@radyalabs.com>
2020-01-11 18:07:58 +0700

    Setup Component & Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { AllPostsComponent } from "./all-posts/all-posts.component";
+import { FollowingComponent } from "./following/following.component";
+import { FavoritesComponent } from "./favorites/favorites.component";
+import { MyPostsComponent } from "./my-posts/my-posts.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "allposts", component: AllPostsComponent },
+  { path: "following", component: FollowingComponent },
+  { path: "favorites", component: FavoritesComponent },
+  { path: "myposts", component: MyPostsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AllPostsComponent } from './all-posts/all-posts.component';
+import { FollowingComponent } from './following/following.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { MyPostsComponent } from './my-posts/my-posts.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { LoginComponent } from './auth/login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AllPostsComponent,
+    FollowingComponent,
+    FavoritesComponent,
+    MyPostsComponent,
+    SignUpComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
